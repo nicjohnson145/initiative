78cc2e71199a00819070ba4c7cc5accbce372a0f
Nic Johnson <nicjohnson145@hotmail.com>
2019-07-28 11:58:09 -0500

    Missed one from my pylint fit

--- initiative/ui/helpful_controller.py
+++ initiative/ui/helpful_controller.py
@@ -8,7 +8,7 @@ class HelpfulController(npyscreen.ActionControllerSimple):
         self.add_action(':h(elp)?', self.display_help, False)
         self.add_action(':.*', self.show_invalid, False)
 
-    def display_help(self, _command_line, _widget_proxy, _live):
+    def display_help(self, command_line, widget_proxy, live):
         self.parent.set_status2_preserve_line(self.parent.COMMAND_TITLE)
         npyscreen.notify_confirm(self.help_message(), title='Help', wide=True)
 
